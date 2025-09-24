164464fd51875a6f4dbea883b602ab8c8d05cad8
sebambus <sebastian.nuxoll@gmail.com>
2025-09-24 12:04:45 -0700

    commented main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,9 @@ int score = 0;
 void playlevel() {
     d.fillDeck();
     d.shuffle();
+
     hand h;
+    // deals 8 cards
     for (int i = 0; i < 8; i++) {
         h.add(d.cards.back());
         d.cards.pop_back();
@@ -21,22 +23,30 @@ void playlevel() {
 
     hand played;    
     while(true) {
+        // update screen
         clear();
         h.print();
         played.print();
         refresh();
+
+        // read keyboard input with switch
         switch (getchar()) {
+            // q - quit
             case 'q':
                 return;
+            // h - left (vim)
             case 'h':
                 h.moveCursor(-1);
                 break;
+            // l - right
             case 'l':
                 h.moveCursor(1);
                 break;
+            // _ - select
             case ' ':
                 h.selected[h.cursor] = !h.selected[h.cursor];
                 break;
+            // p - play
             case 'p':
                 played = hand(h.popSelected());
                 played.cursor = -1;
@@ -46,14 +56,13 @@ void playlevel() {
 }
 
 int main() {
-    setlocale(LC_ALL, "");
-    initscr();             // Start curses modeee
-    start_color();
-    initcolors();
+    setlocale(LC_ALL, "");  // switch to UTF-8??
+    initscr();              // boot ncurses
+    start_color();          // boot ncurses-color
+    initcolors();           // initialize colors for color.h
 
     playlevel();
-    refresh();             // Print it on to the real screen
 
-    endwin();              // End curses mode
+    endwin();               // close ncurses
     return 0;
 }
