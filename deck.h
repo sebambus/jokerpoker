6b6a78db55c0bce55e3df44ffa99b0bb53e86208
sebambus <sebastian.nuxoll@gmail.com>
2025-09-24 12:34:59 -0700

    commented deck.h/cpp

--- deck.h
+++ deck.h
@@ -7,6 +7,8 @@ class deck {
 public:
     std::vector<card> cards;
 
+    // adds the standard 52 poker cards to the deck
     void fillDeck();
+    // shuffles cards using fisher-yates algorithm
     void shuffle();
 };
