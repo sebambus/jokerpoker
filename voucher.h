fbdce9eea52e4391e71b24700a613a47e00d0d9f
sebambus <sebastian.nuxoll@gmail.com>
2025-10-23 18:17:19 -0700

    added todo list to voucher.h

--- voucher.h
+++ voucher.h
@@ -1,38 +1,38 @@
 #pragma once
 
 enum voucher : int {
-    OVERSTOCK,
-    CLEARANCE_SALE,
-    HONE,
-    REROLL_SURPLUS,
-    CRYSTAL_BALL,
-    TELESCOPE,
+    OVERSTOCK,      // needs to be implemented
+    CLEARANCE_SALE, // needs to be implemented
+    HONE,           // needs to be implemented
+    REROLL_SURPLUS, // needs to be implemented
+    CRYSTAL_BALL,   // needs to be implemented
+    TELESCOPE,      // needs to be implemented
     GRABBER,
     WASTEFUL,
-    TAROT_MERCHANT,
-    PLANET_MERCHANT,
-    SEED,
-    BLANK,
-    MAGIC_TRICK,
-    HIEROGLYPH,
-    DIRECTORS_CUT,
-    PAINT_BRUSH,
-    OVERSTOCK_PLUS,
-    LIQUIDATION,
-    GLOW_UP,
-    REROLL_GLUT,
-    OMEN_GLOBE,
-    OBSERVATORY,
+    TAROT_MERCHANT, // needs to be implemented
+    PLANET_MERCHANT,// needs to be implemented
+    SEED,           // needs to be implemented
+    BLANK,          // needs to be implemented
+    MAGIC_TRICK,    // needs to be implemented
+    HIEROGLYPH,     // needs to be implemented
+    DIRECTORS_CUT,  // needs to be implemented
+    PAINT_BRUSH,    // needs to be implemented
+    OVERSTOCK_PLUS, // needs to be implemented
+    LIQUIDATION,    // needs to be implemented
+    GLOW_UP,        // needs to be implemented
+    REROLL_GLUT,    // needs to be implemented
+    OMEN_GLOBE,     // needs to be implemented
+    OBSERVATORY,    // needs to be implemented
     NACHO_TONG,
     RECYCLOMANCY,
-    TAROT_TYCOON,
-    PLANET_TYCOON,
-    MONEY_TREE,
-    ANTIMATTER,
-    ILLUSION,
-    PETROGLYPH,
-    RETCON,
-    PALETTE,
+    TAROT_TYCOON,   // needs to be implemented
+    PLANET_TYCOON,  // needs to be implemented
+    MONEY_TREE,     // needs to be implemented
+    ANTIMATTER,     // needs to be implemented
+    ILLUSION,       // needs to be implemented
+    PETROGLYPH,     // needs to be implemented
+    RETCON,         // needs to be implemented
+    PALETTE,        // needs to be implemented
     VOUCHER_COUNT
 };
 
