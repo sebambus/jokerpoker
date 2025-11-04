fd19bc93b88e3c5cf835c687dfecb31cd0af51c8
sebambus <sebastian.nuxoll@gmail.com>
2025-11-04 06:05:20 -0800

    fixed readcsv() to match the previous change to csv interpretation

--- readcsv.cpp
+++ readcsv.cpp
@@ -8,13 +8,18 @@ const char* readcsv(const char* f, int r, int c) {
     std::ifstream file(f);
     std::string line;
     std::getline(file, line);
-    for (int i = 0; i <= r && std::getline(file, line); i++)
-        if (i == r) {
+    for (int i = 0; i <= r && std::getline(file, line); i++) {
+        if(line.find("NOT COMPLETED") != std::string::npos) {
+            i--;
+            continue;
+        }
+        if(i == r) {
             std::vector<std::string> cols;
             std::string cell;
             for (std::stringstream ss(line); std::getline(ss, cell, ','); cols.push_back(cell));
             if (c >= 0 && c < cols.size()) { res = cols[c]; return res.c_str(); }
         }
+    }
     res = "ERROR";
     return res.c_str();
 }
