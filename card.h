d59c34e9df1fb854e9730808e4de2563bf0ebeee
jacobkuenning <jacob.kuenning@gmail.com>
2025-11-06 15:53:17 -0800

    added enhancements

--- card.h
+++ card.h
@@ -10,10 +10,17 @@ enum suit {
     DIAMOND
 };
 
+enum enhancement {
+    BASE_CARD,
+    BONUS_CARD,
+    MULT_CARD
+};
+
 class card{
 public:
     int cardValue = 0;
     suit cardSuit = SPADE;
+    enhancement cardEnhancement = BASE_CARD;
 
     // new card of value "v" and suit "s"
     card(int v, suit s);
